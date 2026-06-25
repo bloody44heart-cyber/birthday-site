667479efe8225d5c6332eb49a1be9adea2c0277e
bloody44heart-cyber <bloody44heart@gmail.com>
2026-06-25 19:30:37 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const targetDate = new Date("2026-09-04T00:00:00+03:30");
+const targetDate = new Date("2026-08-04T00:00:00+03:30");
 
 const countdownPage = document.querySelector("#countdownPage");
 const birthdayPage = document.querySelector("#birthdayPage");
